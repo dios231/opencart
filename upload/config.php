089a3b798f584201e800a0fab24841a65bb50ea2
diogenis sidiropoulos <sidiropoulos.diogenis@gmail.com>
2017-04-23 12:33:48 +0300

    first commit

---
+++ upload/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart-master/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart-master/upload/');
+
+define('DIR_APPLICATION', 'C:/xampp7/htdocs/opencart-master/upload/catalog/');
+define('DIR_SYSTEM', 'C:/xampp7/htdocs/opencart-master/upload/system/');
+define('DIR_IMAGE', 'C:/xampp7/htdocs/opencart-master/upload/image/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_SYSTEM . 'storage/cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM . 'storage/download/');
+define('DIR_LOGS', DIR_SYSTEM . 'storage/logs/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'storage/modification/');
+define('DIR_UPLOAD', DIR_SYSTEM . 'storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
